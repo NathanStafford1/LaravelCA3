697197a4bf428c737fd31fc6a43114ef1786c1bc
Nathan <d00236171@student.dkit.ie>
2022-04-28 22:31:39 +0100

    Fixed some css display issues

--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -23,6 +23,10 @@ public function user()
     {
         return $this->belongsTo(User::class);
     }
+    public function comments()
+    {
+        return $this->morphMany(Comment::class, 'commentable')->whereNull('parent_id');
+    }
     
     use HasFactory;
 }
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,6 +1,5 @@
 @extends('layouts.app')
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
 @section('content')
     <div class="background-image grid grid-cols-1 m-auto">
         <div class="flex text-gray-100 pt-10">
